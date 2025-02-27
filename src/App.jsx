9b2654cfd1785445e7d3984483cc5a64e6d535cb
Iva Conte <iconte@MacBook-Pro-de-Icc.local>
2025-02-26 23:52:49 -0300

    fix: remove example code

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,8 @@
 import './App.css'
 import Todo from './components/todo/Todo'
 
-const mario = {
-  cliente: 'Mario',
-  idade: 31,
-  compras: [
-    { nome: 'Notebook', preco: 'R$2500' },
-    { nome: 'Geladeira', preco: 'R$3000' },
-    { nome: 'Smartphone', preco: 'R$1500' },
-    { nome: 'Guitarra', preco: 'R$3500' },
-  ],
-  ativa: false
-}
-
 const App = () => {
-  <>
+  return <>
     <Todo/>
   </>
   
